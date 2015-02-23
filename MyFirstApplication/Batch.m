f826a43ac858349cbcce4bf5e30a6f29821d0429
Michael Pryor <pryormic@michaels-mini.home>
2015-02-23 22:12:38 +0000

    Batchers are apparently complete.

--- MyFirstApplication/Batch.m
+++ MyFirstApplication/Batch.m
@@ -13,16 +13,29 @@
     uint _chunksReceived;
     uint _chunkSize;
     uint _numChunksThreshold;
+    uint _timeoutMs;
     ByteBuffer* _partialPacket;
+    NSTimer* _timer;
 }
 
-- (id)initWithOutputSession:(id<OutputSessionBase>)outputSession chunkSize:(uint)chunkSize numChunks:(uint)numChunks andNumChunksThreshold:(uint)numChunksThreshold {
+- (void)onTimeout {
+    NSLog(@"Timed out with chunks received: %ul and threshold: %ul", _chunksReceived, _numChunksThreshold);
+    if(_chunksReceived >= _numChunksThreshold) {
+        @synchronized(_partialPacket) {
+            [_outputSession sendPacket: _partialPacket];
+        }
+    }
+}
+
+- (id)initWithOutputSession:(id<OutputSessionBase>)outputSession chunkSize:(uint)chunkSize numChunks:(uint)numChunks andNumChunksThreshold:(uint)numChunksThreshold andTimeoutMs:(uint)timeoutMs {
     self = [super initWithOutputSession:outputSession];
     if(self) {
         _chunksReceived = 0;
         _numChunksThreshold = numChunksThreshold;
         _chunkSize = chunkSize;
         _partialPacket = [[ByteBuffer alloc] initWithSize:numChunks * chunkSize];
+        _timeoutMs = timeoutMs;
+        _timer = [NSTimer scheduledTimerWithTimeInterval:_timeoutMs target:self selector:@selector(onTimeout) userInfo:nil repeats:NO];
     }
     return self;
 }
@@ -36,7 +49,9 @@
     uint buffPosition = [self getBufferPositionFromChunkId: chunkId];
     
     // Copy contents of chunk packet into partial packet.
-    [_partialPacket addByteBuffer:packet includingPrefix:false atPosition:buffPosition startingFrom:[packet cursorPosition]];
+    @synchronized(_partialPacket) {
+        [_partialPacket addByteBuffer:packet includingPrefix:false atPosition:buffPosition startingFrom:[packet cursorPosition]];
+    }
     
     _chunksReceived += 1;
 }
--- MyFirstApplication/BatcherInput.m
+++ MyFirstApplication/BatcherInput.m
@@ -16,27 +16,42 @@
     uint _chunkSize;
     uint _numChunks;
     uint _numChunksThreshold;
+    uint _timeoutMs;
+    uint _batchRemovalTimeout;
 }
-- (id)initWithOutputSession:(id<OutputSessionBase>)outputSession chunkSize:(uint)chunkSize numChunks:(uint)numChunks andNumChunksThreshold:(uint)numChunksThreshold {
+- (id)initWithOutputSession:(id<OutputSessionBase>)outputSession chunkSize:(uint)chunkSize numChunks:(uint)numChunks andNumChunksThreshold:(uint)numChunksThreshold andTimeoutMs:(uint)timeoutMs {
     self = [super initWithOutputSession:outputSession];
     if(self) {
         _batches = [[NSMutableDictionary alloc] init];
         _chunkSize = chunkSize;
         _numChunks = numChunks;
         _numChunksThreshold = numChunksThreshold;
+        _timeoutMs = timeoutMs;
+        _batchRemovalTimeout = timeoutMs * 3;
     }
     return self;
 }
+
+- (void)onTimeout:(NSNumber*)batchId {
+    NSLog(@"Removing old batch, with ID: %@l", batchId);
+    @synchronized(_batches) {
+        [_batches removeObjectForKey:batchId];
+    }
+}
+
 - (void)onNewPacket:(ByteBuffer*)packet fromProtocol:(ProtocolType)protocol {
     uint batchId = [packet getUnsignedInteger];
     
-    Batch* batch = [_batches objectForKey: [NSNumber numberWithInt:batchId]];
-    if(batch == nil) {
-        batch = [[Batch alloc] initWithOutputSession:_outputSession chunkSize:_chunkSize numChunks:_numChunks andNumChunksThreshold:_numChunksThreshold];
-        [_batches setObject:batch forKey: [NSNumber numberWithInt:batchId]];
+    Batch* batch;
+    @synchronized(_batches) {
+        batch = [_batches objectForKey: [NSNumber numberWithInt:batchId]];
+        if(batch == nil) {
+            batch = [[Batch alloc] initWithOutputSession:_outputSession chunkSize:_chunkSize numChunks:_numChunks andNumChunksThreshold:_numChunksThreshold andTimeoutMs:_timeoutMs];
+            [_batches setObject:batch forKey: [NSNumber numberWithInt:batchId]];
+            [NSTimer scheduledTimerWithTimeInterval:_batchRemovalTimeout target:self selector:@selector(onTimeout:) userInfo:[NSNumber numberWithInt: batchId]    repeats:NO];
+        }
     }
-    
-    
+    [batch onNewPacket:packet fromProtocol:protocol];
 }
 
 
