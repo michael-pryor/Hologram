432a57470ab9aaa3e7ec38e1a2eab80950d6f31b
Michael Pryor <pryormic@Michaels-Mini.home>
2016-03-06 23:50:32 +0000

    Remove bad comment.

--- MyFirstApplication/AudioCompression.m
+++ MyFirstApplication/AudioCompression.m
@@ -82,10 +82,6 @@
         _uncompressedAudioFormat = uncompressedAudioFormat;
         numFramesToDecompressPerOperation = uncompressedAudioFormatEx.framesPerBuffer;
 
-        // Compression does not care about the PCM sample rate.
-        // This is important because sample rate may change when plugging in earphones, or unplugging them.
-        //_uncompressedAudioFormat.mSampleRate = 0;
-
         AudioStreamBasicDescription compressedAudioDescription = {0};
         compressedAudioDescription.mFormatID = kAudioFormatMPEG4AAC;
         compressedAudioDescription.mChannelsPerFrame = 1;
