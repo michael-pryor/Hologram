92086a77bba38f8baf8c5f53f15147f320093ccc
Michael Pryor <pryormic@Michaels-Mini.home>
2016-07-04 13:51:24 +0100

    Missing EULA storyboard.

---
+++ MyFirstApplication/EndUserLicenseAgreementViewController.h
@@ -0,0 +1,9 @@
+//
+// Created by Michael Pryor on 04/07/2016.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface EndUserLicenseAgreementViewController : UIViewController
+@end
---
+++ MyFirstApplication/EndUserLicenseAgreementViewController.m
@@ -0,0 +1,31 @@
+//
+// Created by Michael Pryor on 04/07/2016.
+//
+
+#import "EndUserLicenseAgreementViewController.h"
+#import "Threading.h"
+#import "SocialState.h"
+
+
+@implementation EndUserLicenseAgreementViewController {
+    __weak IBOutlet UITextView *_eulaTextView;
+}
+
+- (void)viewDidLoad {
+    [_eulaTextView.layer setBorderColor:[[UIColor blackColor] CGColor]];
+    [_eulaTextView.layer setBorderWidth:0.5];
+}
+
+- (IBAction)onAcceptButtonPress:(id)sender {
+    [[SocialState getSocialInstance] persistHasAcceptedEula];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (IBAction)onCancelButtonPress:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+// Scroll to top on UITextViews.
+- (void)viewDidLayoutSubviews {
+    [_eulaTextView setContentOffset:CGPointZero animated:NO];
+}
+@end
