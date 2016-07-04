79c623e28515199ca66c638a86befeefdd8c45a1
Michael Pryor <pryormic@Michaels-Mini.home>
2016-07-04 19:43:30 +0100

    Missing files.

---
+++ MyFirstApplication/HelpViewController.m
@@ -0,0 +1,14 @@
+//
+// Created by Michael Pryor on 04/07/2016.
+//
+
+#import "HelpViewController.h"
+
+
+@implementation HelpViewController {
+
+}
+- (IBAction)onDoneButtonPressed:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
